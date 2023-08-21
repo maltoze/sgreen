b19105dd9bb8c174bedf6906c81b25c0a379bf9e
maltoze <atonezzz@gmail.com>
2023-08-21 13:34:22 +0800

    chore: remove popup

--- src/manifest.ts
+++ src/manifest.ts
@@ -3,7 +3,6 @@ import pkg from '../package.json'
 const manifest: Partial<chrome.runtime.ManifestV3> = {
   action: {
     default_icon: 'icon.png',
-    // default_popup: 'src/entries/popup/index.html',
   },
   background: {
     service_worker: 'src/entries/background/main.ts',
