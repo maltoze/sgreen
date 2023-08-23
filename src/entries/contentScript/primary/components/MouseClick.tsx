121c62cb1abc3b2e2ec04b7c80a8be2c5da08b94
maltoze <atonezzz@gmail.com>
2023-08-23 09:02:12 +0800

    feat: handle pointer move

--- src/entries/contentScript/primary/components/MouseClick.tsx
+++ src/entries/contentScript/primary/components/MouseClick.tsx
@@ -1,27 +1,44 @@
 import { AnimatePresence, motion } from 'framer-motion'
-import { useEffect, useState } from 'react'
+import { useCallback, useEffect, useState } from 'react'
 
 export default function MouseClick() {
   const [position, setPosition] = useState({ x: 0, y: 0 })
   const [isPointerDown, setIsPointerDown] = useState(false)
 
-  function handleClick(e: MouseEvent) {
+  function handlePointerDown(e: PointerEvent) {
     setPosition({ x: e.clientX, y: e.clientY })
     setIsPointerDown(true)
   }
 
+  const handlePointerMove = useCallback(
+    (e: PointerEvent) => {
+      if (isPointerDown) {
+        setPosition({ x: e.clientX, y: e.clientY })
+      }
+    },
+    [isPointerDown],
+  )
+
+  function handlePointerUp(_e: PointerEvent) {
+    setIsPointerDown(false)
+  }
+
   useEffect(() => {
-    document.addEventListener('click', handleClick)
+    document.addEventListener('pointerdown', handlePointerDown)
+    document.addEventListener('pointerup', handlePointerUp)
+    document.addEventListener('pointermove', handlePointerMove)
     return () => {
-      document.removeEventListener('click', handleClick)
+      document.removeEventListener('pointerdown', handlePointerDown)
+      document.removeEventListener('pointerup', handlePointerUp)
+      document.removeEventListener('pointermove', handlePointerMove)
     }
-  }, [])
+  }, [handlePointerMove])
 
   return (
     <AnimatePresence>
       {isPointerDown && (
         <motion.div
-          className="pointer-events-none fixed z-[2147483647] h-4 w-4 rounded-full bg-green-500/50"
+          className="pointer-events-none fixed z-[2147483647] h-4 w-4 rounded-full bg-primary/60"
           style={{ left: position.x, top: position.y }}
           initial={{
             scale: 0,
@@ -29,10 +46,11 @@ export default function MouseClick() {
             translateX: '-50%',
             translateY: '-50%',
           }}
-          animate={{ scale: 3, opacity: 1, transition: { duration: 0 } }}
-          exit={{ scale: 0, opacity: 0, transition: { duration: 0.5, ease: 'linear' } }}
-          onAnimationComplete={() => {
-            setIsPointerDown(false)
+          animate={{ scale: 2, opacity: 1, transition: { duration: 0 } }}
+          exit={{
+            scale: 0,
+            opacity: 0,
+            transition: { duration: 0.3, ease: 'linear' },
           }}
         ></motion.div>
       )}
