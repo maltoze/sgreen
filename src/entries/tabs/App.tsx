dd344c4449d4c04b5c39d67b011cebd060d838e9
maltoze <atonezzz@gmail.com>
2023-08-23 11:24:17 +0800

    feat: add confetti effect

--- src/entries/tabs/App.tsx
+++ src/entries/tabs/App.tsx
@@ -1,4 +1,5 @@
 import { useRef } from 'react'
+import Confetti from 'react-confetti'
 
 const params = new URLSearchParams(location.search)
 const videoUrl = params.get('videoUrl')
@@ -11,14 +12,17 @@ export default function App() {
   }
 
   return (
-    <main className="flex h-screen items-center justify-center bg-gradient-to-tr from-purple-100 via-green-50 to-pink-100 p-10 lg:p-20">
-      <video
-        className="max-h-full rounded-lg shadow-[0_4px_6px_-1px_rgb(0_0_0_/0.1),0_2px_4px_-2px_rgb(0_0_0/0.1),0_-2px_4px_-1px_rgb(0_0_0_/0.1)]"
-        controls
-        ref={videoRef}
-        src={videoUrl}
-        preload="metadata"
-      />
-    </main>
+    <>
+      <Confetti recycle={false} gravity={0.5} numberOfPieces={300} />
+      <main className="flex h-screen items-center justify-center bg-gradient-to-tr from-purple-100 via-green-50 to-pink-100 p-10 lg:p-20">
+        <video
+          className="max-h-full rounded-lg shadow-[0_4px_6px_-1px_rgb(0_0_0_/0.1),0_2px_4px_-2px_rgb(0_0_0/0.1),0_-2px_4px_-1px_rgb(0_0_0_/0.1)]"
+          controls
+          ref={videoRef}
+          src={videoUrl}
+          preload="metadata"
+        />
+      </main>
+    </>
   )
 }
